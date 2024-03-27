3c642bb2fc5dc4cf1e4a0e690a456c3f87404bc6
dom <limoudom2001@gmail.com>
2024-03-27 14:24:54 +1100

    Initial POST and GET author routes

--- server.js
+++ server.js
@@ -6,14 +6,17 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL);
@@ -23,5 +26,6 @@ db.once("open", () => console.log("Connected to Mongoose"));
 
 // at the root of our site use indexRouter
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 3000);
